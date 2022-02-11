2ff470a1547adb97f54946c95050ae6334a35331
Josh Frandsen <joshfrandsen@Joshs-MBP.home>
2022-02-11 14:31:40 -0800

    added file path to lualine

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -51,6 +51,41 @@ return packer.startup(function(use)
 	use({
 		"nvim-lualine/lualine.nvim",
 		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = true,
+					theme = "auto",
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+					disabled_filetypes = {},
+					always_divide_middle = true,
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = {
+						function()
+							return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+								.. vim.fn.expand(string.gsub(vim.api.nvim_buf_get_name(0), vim.loop.cwd(), ""))
+						end,
+					},
+					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = { "filename" },
+					lualine_x = { "location" },
+					lualine_y = {},
+					lualine_z = {},
+				},
+				tabline = {},
+				extensions = {},
+			})
+		end,
 	})
 
 	require("lualine").setup({
