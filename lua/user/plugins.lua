adad67b2506c4977a3fcfe57bdb5d9576ebbbf69
Josh Frandsen <joshfrandsen@Joshs-MBP.home>
2022-02-28 14:42:35 -0800

    actually changed icons for gitsigns

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -132,31 +132,34 @@ return packer.startup(function(use)
 		},
 		config = function()
 			require("gitsigns").setup({
-				add = { hl = "GitSignsAdd", text = "⏽", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-				delete = {
-					hl = "GitSignsDelete",
-					text = "_",
-					numhl = "GitSignsDeleteNr",
-					linehl = "GitSignsDeleteLn",
+				signs = {
+					add = { hl = "GitSignsAdd", text = "⏽", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+					change = {
+						hl = "GitSignsChange",
+						text = "⏽",
+						numhl = "GitSignsChangeNr",
+						linehl = "GitSignsChangeLn",
+					},
+					delete = {
+						hl = "GitSignsDelete",
+						text = "⏽",
+						numhl = "GitSignsDeleteNr",
+						linehl = "GitSignsDeleteLn",
+					},
+					topdelete = {
+						hl = "GitSignsDelete",
+						text = "⏽",
+						numhl = "GitSignsDeleteNr",
+						linehl = "GitSignsDeleteLn",
+					},
+					changedelete = {
+						hl = "GitSignsChange",
+						text = "~",
+						numhl = "GitSignsChangeNr",
+						linehl = "GitSignsChangeLn",
+					},
 				},
-				topdelete = {
-					hl = "GitSignsDelete",
-					text = "‾",
-					numhl = "GitSignsDeleteNr",
-					linehl = "GitSignsDeleteLn",
-				},
-				changedelete = {
-					hl = "GitSignsChange",
-					text = "~",
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
-				},
-				change = {
-					hl = "GitSignsChange",
-					text = "⏽",
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
-				},
+				signcolumn = true,
 			})
 		end,
 	})
