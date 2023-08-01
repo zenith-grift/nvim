0265cd77da0618dda748baffbdf5ab444a96d985
zenith-grift <josh.frandsen3@gmail.com>
2023-07-31 20:23:38 -1000

    adding some remaps

--- lua/no_gosu/remap.lua
+++ lua/no_gosu/remap.lua
@@ -2,3 +2,19 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jk", "<esc>")
+
+-- move visual blocks
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- J brings up below line but stays in-place
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- keep search and page down/up in the middle
+vim.keymap.set("n", "<C-d>", '<C-d>zz')
+vim.keymap.set("n", "<C-u>", '<C-u>zz')
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- keep buffer with last coppied instead of replacing
+vim.keymap.set("x", "p", [["_dP]])
