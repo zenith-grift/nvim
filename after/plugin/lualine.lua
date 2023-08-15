dd812a0ba9584fce071841b097b0468f9f352eaf
zenith-grift <josh.frandsen3@gmail.com>
2023-08-14 20:25:48 -1000

    lualine, gitsigns, autoslcose

---
+++ after/plugin/lualine.lua
@@ -0,0 +1,52 @@
+local function cwd()
+    local file = vim.api.nvim_buf_get_name(0)
+    local path = os.getenv("PWD")
+    local root = path:match(".+[/\\]([^/\\]+)$")
+    if root == nil then
+        root = ""
+    end
+
+    return root .. "" .. string.gsub(file, path, "")
+end
+
+require('lualine').setup {
+    options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+            "NvimTree"
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        },
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff' },
+        lualine_c = { cwd },
+        lualine_x = { 'diagnostics', 'encoding', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {}
+}
